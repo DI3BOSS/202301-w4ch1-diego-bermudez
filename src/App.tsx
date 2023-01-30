bae9b9685eae293e43748d347e3077a3382846a2
DI3BOSS <diegofbermudez@gmail.com>
2023-01-30 21:22:19 +0100

    Add title and controls on header for button positioning

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,20 @@
+import Button from "./components/button/Button";
+
 function App() {
-  return <div className="App"></div>;
+  return (
+    <>
+      <header className="main-header">
+        <h1 className="main-title">The pointing gentlemen</h1>
+      </header>
+
+      <section className="controls">
+        <>
+          <p className="info">0 gentlemen pointing at you</p>
+          {Button}
+        </>
+      </section>
+    </>
+  );
 }
 
 export default App;
